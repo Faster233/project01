1df615c93feeefe0b153ac4bbc6eb08996e08ce4
Faster <Faster@shgogo.com>
2021-10-08 19:44:20 +0800

    first commit

---
+++ src/main/java/com/work/git/GitTest.java
@@ -0,0 +1,10 @@
+package com.work.git;/*
+@date   2021/10/8 - 19:39
+@SH     Let's go! Fuck Everything!
+*/
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git !");
+    }
+}
